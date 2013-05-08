db1d037d49eae73f19b529e734062abddd1e194c
krishna <skyblue_flare@yahoo.co.jp>
2013-05-08 18:56:44 +0900

    new. P.34 int array sample

---
+++ int_array_34.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int data[10], i;
+
+    for(i = 0; i < 10; i++)
+        data[i] = i * 10;
+
+    for(i = 9; i >= 0; i--)
+        printf("data[%d] --> %d\n", i, data[i]);
+}
