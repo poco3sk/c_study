55e40f907cd428c99985c7a13b73adc094e4bd67
krishna <skyblue_flare@yahoo.co.jp>
2013-05-09 00:31:17 +0900

    new. P.96 variable init

---
+++ chap_2/variable_init_96.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+static int ia = 12 * 31;
+float fa = 0.456;
+
+int main()
+{
+    int     ib  = 128;
+    float   fb  = 456.78 / 100;
+    char    ca  = 'A',
+            cb  = '\n';
+
+    printf("start --------------\n");
+    printf("ia --> %d ib --> %d\n", ia, ib);
+    printf("fa --> %f fb --> %f\n", fa, fb);
+    printf("ca --> %c cb --> %c\n", ca, cb);
+    printf("---------------- end\n");
+
+    return 0;
+}
