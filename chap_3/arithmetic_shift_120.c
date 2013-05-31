ac37c0bd350eae4e654574205944cac6a93d672d
krishna <skyblue_flare@yahoo.co.jp>
2013-05-31 17:00:50 +0900

    new, P.120 arithmetic shift

---
+++ chap_3/arithmetic_shift_120.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void) {
+    int a = -1;
+    void hex_print(int);
+
+    hex_print(a);
+
+    a = a << 4;
+    hex_print(a);
+
+    a = a >> 4;
+    hex_print(a);
+
+    return 0;
+}
+
+void hex_print(int x) {
+    printf("(%10d) --> %08X\n", x, x);
+}
